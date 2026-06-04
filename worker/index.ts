ea3ab0606005d023375193f0c8b6268d53c317c0
Jiaming Kang <42668274+xyspg@users.noreply.github.com>
2026-06-04 09:59:53 -0400

    add cloudflare DO

--- worker/index.ts
+++ worker/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { tripData } from "../src/trip/tripData";
 import type { Trip } from "../src/trip/types";
+import { DurableObject } from "cloudflare:workers";
 
 const app = new Hono();
 
